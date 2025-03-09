0ba48638729760298150d41b5abe7a74a541e0a7
Qwell10 <mihal.chaloupka@seznam.cz>
2025-03-09 19:43:02 +0100

    ...

--- src/main/java/com/FrumoStore/config/SecurityConfig.java
+++ src/main/java/com/FrumoStore/config/SecurityConfig.java
@@ -1,14 +1,13 @@
 package com.FrumoStore.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -18,14 +17,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/user/register").permitAll() // Allow unauthenticated access to /user/register
-                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll() // Allow unauthenticated access to Swagger UI
-                        .anyRequest().authenticated() // All other requests require authentication
-                )
-                .httpBasic(withDefaults()); // Or formLogin, depending on your needs.
+                        .requestMatchers(HttpMethod.POST, "/user/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/user/login").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
+                        .anyRequest().authenticated()
+                );
 
         return http.build();
     }
-
 }
